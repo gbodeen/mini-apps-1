c665dbb3a8620c31d91048e053150c18c9b72dd8
Gabriel Bodeen <gabebodeen@gmail.com>
2018-12-10 14:46:55 -0600

    (feat) Game logic added.

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,27 +1,60 @@
 
-let winner = '';
-let player = 'X';
+let game = true;
+let player1 = true;
 
 const handleCellClick = (e) => {
-    // Check if there's a game going on.
-    if (winner) return;
-    // Check if the cell is available.
-    if (e.target.innerText) return;
-    console.log(e, e.target);
-    // Check for whose turn it is. Put a letter in the cell.
-    e.target.innerText = player;
-    // Check for a win or a tie.
+  if (!game) return;
+  if (e.target.innerText) return;
+
+  e.target.innerText = player1 ? 'X' : 'O';
+  player1 = !player1;
+  document.getElementById('winner-area').innerText = 'The game is on!\nIt\'s '
+    + (player1 ? 'X' : 'O') + '\'s turn.';
+
+  let winLine;
+  for (let cellClass of e.target.classList) {
+    winLine = [];
+    for (let cell of document.getElementsByClassName(cellClass)) {
+      winLine.push(cell.innerText || null);
+    }
+    if (winLine.every(s => s === 'X')) {
+      setWinner('X');
+    }
+    else if (winLine.every(s => s === 'O')) {
+      setWinner('O');
+    }
+    else if (cellClass === 'cell' && winLine.every(s => s)) {
+      setWinner(null);
+    }
+  }
 }
 
+const setWinner = (outcome) => {
+  game = false;
+  if (outcome) {
+    document.getElementById('winner-area').innerText = outcome + ' is the winner!\n';
+    document.getElementById(outcome + 'wins').innerText++;
+  } else {
+    document.getElementById('winner-area').innerText = 'X and O have tied.\n';
+  }
+}
+
+const handleReset = () => {
+  game = true;
+  player1 = true;
+  for (let cell of document.getElementsByClassName('cell')) {
+    cell.innerText = '';
+  }
+  document.getElementById('winner-area').innerText = 'The game is on!\nIt\'s '
+    + (player1 ? 'X' : 'O') + '\'s turn.';
+}
 
 
 
 const cells = document.getElementsByClassName('cell');
 
 for (let cell of cells) {
-    cell.addEventListener('click', handleCellClick);
+  cell.addEventListener('click', handleCellClick);
 }
 
-
-
-
+document.getElementById('reset-button').addEventListener('click', handleReset);
