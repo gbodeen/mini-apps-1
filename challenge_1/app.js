d22924020e2dcf95af13dfce124894867d2511c9
Gabriel Bodeen <gabebodeen@gmail.com>
2018-12-10 15:45:45 -0600

    Consolidate display functions to gameDisplay object.

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -3,14 +3,11 @@ const gameState = {
 
   isGameOver: false,
   isXsTurn: true,
-  player1: '',
-  player2: '',
 
-  toggleSquare: function (e) {
-    e.target.innerText = gameState.isXsTurn ? 'X' : 'O';
+  markSquare: function (e) {
+    gameDisplay.showMark(e);
     gameState.isXsTurn = !gameState.isXsTurn;
-    document.getElementById('winner-area').innerText = 'The game is on!\nIt\'s '
-      + (gameState.isXsTurn ? 'X' : 'O') + '\'s turn.';
+    gameDisplay.updateTurn();
   },
 
   checkForWinner: function (e) {
@@ -20,35 +17,66 @@ const gameState = {
       for (let square of document.getElementsByClassName(squareClass)) {
         winLine.push(square.innerText || null);
       }
-      if (winLine.every(s => s === 'X')) {
-        setWinner('X');
+      if (winLine.every(mark => mark === 'X')) {
+        gameState.isGaveOver = true;
+        gameDisplay.showWinner('X', gameDisplay.player1);
       }
-      else if (winLine.every(s => s === 'O')) {
-        setWinner('O');
+      else if (winLine.every(mark => mark === 'O')) {
+        gameState.isGaveOver = true;
+        gameDisplay.showWinner('O', gameDisplay.player2);
       }
-      else if (squareClass === 'square' && winLine.every(s => s)) {
-        setWinner(null);
+      else if (squareClass === 'square' && winLine.every(mark => mark)) {
+        gameState.isGaveOver = true;
+        gameDisplay.showWinner(null);
       }
+    }
+  }
+}
+
+const gameDisplay = {
+
+  player1: '',
+  player2: '',
+
+  showPlayerNames: function () {
+    gameDisplay.player1 = prompt('Enter player 1\'s name: ');;
+    gameDisplay.player2 = prompt('Enter player 2\'s name: ');;
+    document.getElementById('Xname').innerText = gameDisplay.player1;
+    document.getElementById('Oname').innerText = gameDisplay.player2;
+  },
+
+  showMark: function (e) {
+    e.target.innerText = gameState.isXsTurn ? 'X' : 'O';
+  },
+
+  showWinner: function (xo, player) {
+    if (player) {
+      document.getElementById('headline').innerText = player + ' (' + xo + ') is the winner!\n';
+      document.getElementById(xo + 'wins').innerText++;
+    } else {
+      document.getElementById('headline').innerText = 'X and O have tied.\n';
+    }
+  },
+
+  updateTurn: function () {
+    document.getElementById('headline').innerText = 'The game is on!\nIt\'s '
+      + (gameState.isXsTurn ? 'X' : 'O') + '\'s turn.';
+  },
+
+  clearBoard: function () {
+    for (let square of document.getElementsByClassName('square')) {
+      square.innerText = '';
     }
   }
 }
 
 const handleSquareClick = (e) => {
   if (!gameState.isGameOver && !e.target.innerText) {
-    gameState.toggleSquare(e);
+    gameState.markSquare(e);
     gameState.checkForWinner(e);
   }
 }
 
-const setWinner = (outcome) => {
-  gameState.isGaveOver = true;
-  if (outcome) {
-    document.getElementById('winner-area').innerText = outcome + ' is the winner!\n';
-    document.getElementById(outcome + 'wins').innerText++;
-  } else {
-    document.getElementById('winner-area').innerText = 'X and O have tied.\n';
-  }
-}
 
 const handleReset = () => {
   gameState.isGaveOver = false;
@@ -67,15 +95,8 @@ for (let square of squares) {
   square.addEventListener('click', handleSquareClick);
 }
 
-document.getElementById('reset-button').addEventListener('click', handleReset);
+document.getElementById('reset-button').addEventListener('click', gameDisplay.clearBoard);
 
-const setPlayerNames = () => {
-  let Xname = prompt('Enter player 1\'s name: ');;
-  let Oname = prompt('Enter player 2\'s name: ');;
-  document.getElementById('Xname').innerText = Xname;
-  document.getElementById('Oname').innerText = Oname;
-  gameState.player1 = Xname;
-  gameState.player2 = Oname;
-}
 
-setPlayerNames();
+
+gameDisplay.showPlayerNames();
