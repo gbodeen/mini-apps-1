40c98c30b1c14ccd083813e9c9f7d5632003b5c0
Gabriel Bodeen <gabebodeen@gmail.com>
2018-12-10 12:45:47 -0600

    Add EventListener to cells of board.

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,27 @@
 
+let winner = '';
+let player = 'X';
+
+const handleCellClick = (e) => {
+    // Check if there's a game going on.
+    if (winner) return;
+    // Check if the cell is available.
+    if (e.target.innerText) return;
+    console.log(e, e.target);
+    // Check for whose turn it is. Put a letter in the cell.
+    e.target.innerText = player;
+    // Check for a win or a tie.
+}
+
+
+
+
+const cells = document.getElementsByClassName('cell');
+
+for (let cell of cells) {
+    cell.addEventListener('click', handleCellClick);
+}
+
+
+
 
