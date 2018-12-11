52f92bac5e361fce0f012bb5d984a45eedcd5b6f
Gabriel Bodeen <gabebodeen@gmail.com>
2018-12-11 17:23:49 -0600

    Switch from default Submit to single-page-app (no reloading) via XHR.

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,19 +1,34 @@
 const displayCSV = (resText) => {
-    document.getElementById('csv-area').innerText = resText;
+  document.getElementById('csv-area').innerText = resText;
 };
 
 const reader = new FileReader();
 
 const handleFileSelect = (e) => {
-    reader.readAsText(e.target.files[0]);
-    reader.onload = () => {
-        document.getElementById('json').value = reader.result;
-    };
+  reader.readAsText(e.target.files[0]);
+  reader.onload = () => {
+    document.getElementById('json').value = reader.result;
+  };
 }
 
-
-fetch('/csv')
+const fetchCSV = () => {
+  fetch('/csv')
     .then(res => res.text())
     .then(text => displayCSV(text));
+}
 
-document.getElementById('picker').addEventListener('change', handleFileSelect);
+const addListeners = () => {
+  document.getElementById('picker').addEventListener('change', handleFileSelect);
+
+  document.getElementById("submit").addEventListener("click", function (e) {
+    e.preventDefault();
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', '/json');
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(document.getElementById('json').value);
+    fetchCSV();
+  });
+}
+
+fetchCSV();
+addListeners();
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -7,70 +7,66 @@ const data = { objects: [], keys: [], csv: '' };
 app.use(express.static('client'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/client/index.html');
+  res.sendFile(__dirname + '/client/index.html');
 });
 
-app.post('/json', express.urlencoded({ extended: true }), (req, res) => {
-    console.log(req.body);
-    handleNewJSON(req.body.json);
-    res.redirect('/');
+app.post('/json', express.json(), (req, res) => {
+  handleNewObject(req.body)
+  res.redirect('/');
 });
 
 app.get('/csv', (req, res) => {
-    res.send(data.csv);
+  res.send(data.csv);
 })
 
 app.listen(port, hostname);
 
-const handleNewJSON = (json) => {
-    const newObj = tryParsingJSON(json);
-    if (newObj) {
-        data.objects = updateDataObjects(newObj, data.objects);
-        data.keys = updateDataKeys(data.objects);
-        data.csv = updateDataCSV(data.keys, data.objects);
-    }
+const handleNewObject = (newObj) => {
+  data.objects = updateDataObjects(newObj, data.objects);
+  data.keys = updateDataKeys(data.objects);
+  data.csv = updateDataCSV(data.keys, data.objects);
 }
 
 const tryParsingJSON = (maybeJSON = '') => {
-    try {
-        json = JSON.parse(maybeJSON);
-    }
-    catch (err) {
-        console.log('BROKEN JSON', maybeJSON);
-        json = null;
-    }
-    return json;
+  try {
+    json = JSON.parse(maybeJSON);
+  }
+  catch (err) {
+    console.log('BROKEN JSON', maybeJSON);
+    json = null;
+  }
+  return json;
 };
 
 const updateDataObjects = (newRootObj, objects) => {
-    objects.push(newRootObj);
-    if (newRootObj.hasOwnProperty('children')) {
-        newRootObj.children.forEach(child => {
-            updateDataObjects(child, objects);
-        });
-    }
-    return objects;
+  objects.push(newRootObj);
+  if (newRootObj.hasOwnProperty('children')) {
+    newRootObj.children.forEach(child => {
+      updateDataObjects(child, objects);
+    });
+  }
+  return objects;
 };
 
 const updateDataKeys = (objects) => {
-    const withRepeats = objects.reduce((keys, obj) => {
-        return keys.concat(Object.keys(obj));
-    }, []);
-    const withoutRepeats = new Set(withRepeats);
-    withoutRepeats.delete('children');
-    return [...withoutRepeats];
+  const withRepeats = objects.reduce((keys, obj) => {
+    return keys.concat(Object.keys(obj));
+  }, []);
+  const withoutRepeats = new Set(withRepeats);
+  withoutRepeats.delete('children');
+  return [...withoutRepeats];
 }
 
 const updateDataCSV = (keys, objects) => {
-    const header = keys.join(',');
-    const body = [header];
-    let row;
-    for (let obj of objects) {
-        row = [];
-        for (let key of keys) {
-            row.push(obj.hasOwnProperty(key) ? obj[key] : '');
-        }
-        body.push(row.join(','));
+  const header = keys.join(',');
+  const body = [header];
+  let row;
+  for (let obj of objects) {
+    row = [];
+    for (let key of keys) {
+      row.push(obj.hasOwnProperty(key) ? obj[key] : '');
     }
-    return body.join('\n');
+    body.push(row.join(','));
+  }
+  return body.join('\n');
 };
