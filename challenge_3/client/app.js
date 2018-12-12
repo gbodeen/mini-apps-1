c171815b6c71b4b700a4b015139cf947c5098620
Gabriel Bodeen <gabebodeen@gmail.com>
2018-12-12 12:14:34 -0600

    Home page subcomponent with checkout button now renders.

--- challenge_3/client/app.js
+++ challenge_3/client/app.js
@@ -10,10 +10,83 @@ class App extends React.Component {
   }
 
   render() {
+    switch (this.state.page) {
+      case 1:
+        return React.createElement(F1, null);
+      case 2:
+        return React.createElement(F2, null);
+      case 3:
+        return React.createElement(F3, null);
+      default:
+        return React.createElement(Home, null);
+    }
+  }
+}
+
+class Home extends React.Component {
+  // a Checkout button, which when clicked, takes the user to the first of several forms
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
     return React.createElement(
-      'div',
+      "div",
       null,
-      'Now there\'s React with state and props!'
+      React.createElement(
+        "button",
+        { type: "button" },
+        "Checkout"
+      )
+    );
+  }
+}
+
+class F1 extends React.Component {
+  // F1 collects name, email, and password for account creation.
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return React.createElement(
+      "div",
+      null,
+      "Work in progress. This is F1."
+    );
+  }
+}
+
+class F2 extends React.Component {
+  // F2 collects ship to address (line 1, line 2, city, state, zip code) and phone number.
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return React.createElement(
+      "div",
+      null,
+      "Work in progress. This is F2."
+    );
+  }
+}
+
+class F3 extends React.Component {
+  // F3 collects credit card #, expiry date, CVV, and billing zip code.
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return React.createElement(
+      "div",
+      null,
+      "Work in progress. This is F3."
     );
   }
 }
--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -10,9 +10,64 @@ class App extends React.Component {
   }
 
   render() {
-    return (
-      <div>Now there's React with state and props!</div>
-    )
+    switch (this.state.page) {
+      case (1):
+        return <F1 />;
+      case (2):
+        return <F2 />;
+      case (3):
+        return <F3 />;
+      default:
+        return <Home />;
+    }
+  }
+}
+
+class Home extends React.Component {
+  // a Checkout button, which when clicked, takes the user to the first of several forms
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div><button type="button">Checkout</button></div>;
+  }
+}
+
+class F1 extends React.Component {
+  // F1 collects name, email, and password for account creation.
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div>Work in progress. This is F1.</div>;
+  }
+}
+
+class F2 extends React.Component {
+  // F2 collects ship to address (line 1, line 2, city, state, zip code) and phone number.
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div>Work in progress. This is F2.</div>;
+  }
+}
+
+class F3 extends React.Component {
+  // F3 collects credit card #, expiry date, CVV, and billing zip code.
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div>Work in progress. This is F3.</div>;
   }
 }
 
