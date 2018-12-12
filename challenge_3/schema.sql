0ab765a0b2bb60aeea9bdf2889638eb486c9d31f
Gabriel Bodeen <gabebodeen@gmail.com>
2018-12-12 15:21:11 -0600

    Basic DB schema.

---
+++ challenge_3/schema.sql
@@ -0,0 +1,21 @@
+DROP DATABASE IF EXISTS checkout;
+CREATE DATABASE checkout;
+USE checkout;
+
+DROP TABLE IF EXISTS orders;
+CREATE TABLE orders (
+  id INT NOT NULL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255),
+  password VARCHAR(255),
+  address1 VARCHAR(255) NOT NULL,
+  address2 VARCHAR(255),
+  city VARCHAR(255) NOT NULL,
+  state VARCHAR(2) NOT NULL,
+  zip VARCHAR(5) NOT NULL,
+  phone VARCHAR(30),
+  cc VARCHAR(25) NOT NULL,
+  expiry VARCHAR(7) NOT NULL,
+  cvv VARCHAR(4) NOT NULL,
+  billzip VARCHAR(5) NOT NULL
+);
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
 const port = process.env.port || 3000;
+const mysql = require('mysql');
 
 app.use(express.static('public'));
 
 app.listen(port, 'localhost');
 
 
+const dbOptions = {
+  'username': 'root',
+  'password': '',
+  'database': 'checkout'
+}
+
+dbConnection = mysql.createConnection(dbOptions);
 
 
