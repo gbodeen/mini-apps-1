fd638e21688d18e0c4abf60454f9ff9305f559ae
Gabriel Bodeen <gabebodeen@gmail.com>
2018-12-12 12:28:11 -0600

    Simplified static serving of index.html and app.js.

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -2,11 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.port || 3000;
 
-app.use('/client', express.static(__dirname + '/client'))
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
-});
+app.use(express.static('public'));
 
 app.listen(port, 'localhost');
 
