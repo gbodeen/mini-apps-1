41e96e938e4687f2d0b5fd212cb4da6d29c284c9
Gabriel Bodeen <gabebodeen@gmail.com>
2018-12-12 10:40:00 -0600

    Server servers index.html and app.jsx.

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,17 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
 
 
+ReactDOM.render(document.getElementById('root'));
+
+class Cart extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>Now there's React!</div>
+    )
+  }
+}
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const port = process.env.port || 3000;
 
+app.use('/client', express.static(__dirname + '/client'))
+
 app.get('/', (req, res) => {
-  res.send('Hello there');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 app.listen(port, 'localhost');
