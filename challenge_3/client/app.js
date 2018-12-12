bb4cfb9e9abf8333e149c83f5f51b388288fbb55
Gabriel Bodeen <gabebodeen@gmail.com>
2018-12-12 11:51:42 -0600

    Change App from function to class Component with state.

--- challenge_3/client/app.js
+++ challenge_3/client/app.js
@@ -1,11 +1,21 @@
 // import React from 'react';
 // import ReactDOM from 'react-dom';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: 0
+    };
+  }
 
-const App = () => React.createElement(
-  'div',
-  null,
-  'Now there\'s React!'
-);
+  render() {
+    return React.createElement(
+      'div',
+      null,
+      'Now there\'s React with state and props!'
+    );
+  }
+}
 
 ReactDOM.render(React.createElement(App, null), document.getElementById('root'));
--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,9 +1,19 @@
 // import React from 'react';
 // import ReactDOM from 'react-dom';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: 0
+    }
+  }
 
-const App = () => (
-  <div>Now there's React!</div>
-);
+  render() {
+    return (
+      <div>Now there's React with state and props!</div>
+    )
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
