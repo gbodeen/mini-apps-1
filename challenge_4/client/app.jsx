697085873dbb3439d7bfe558e30441b8717eddf7
Gabriel Bodeen <gabebodeen@gmail.com>
2018-12-13 11:05:15 -0600

    The subcomponents also display now.

--- challenge_4/client/app.jsx
+++ challenge_4/client/app.jsx
@@ -1,9 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => (
-  <div>React is on!</div>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: '',
+      board: []
+    };
+
+    this.dropPiece = this.dropPiece.bind(this);
+    this.componentDidMount = this.componentDidMount.bind(this);
+  }
+
+  componentDidMount() {
+    const board = [];
+    for (let row = 0; row < 6; row++) {
+      board.push([]);
+      for (let col = 0; col < 7; col++) {
+        board[row].push(0);
+      }
+    }
+    this.setState({ board });
+  }
+
+  dropPiece() {
+    console.log('This should drop a piece.')
+  }
+
+  render() {
+    return (
+      <>
+        <InfoArea message={this.state.message} />
+        <DropArea dropPiece={this.dropPiece} />
+        <Board board={this.state.board} />
+      </>
+    )
+  }
+}
+
+const InfoArea = ({ message }) => (
+  <div>This will be the info display area.</div>
 )
+
+const DropArea = ({ dropPiece }) => (
+  <div>This will be where you click to drop a piece.</div>
+)
+
+const Board = ({ board }) => (
+  <div>This will be where the board displays.</div>
+)
+
 
 const root = document.getElementById('root');
 ReactDOM.render(<App />, root);
